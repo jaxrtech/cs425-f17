bf59087a29ebb80640b082c3729578406a84e6fc
Taylor Berg <me@tman0.com>
2017-12-04 15:42:58 -0600

    fix: buttons don't have href

--- sample_data/aero_class.sql
+++ sample_data/aero_class.sql
@@ -1,5 +1,5 @@
 INSERT INTO class (id, display_name, fee) VALUES
-  (1, 'Economy', '0.00'),
-  (2, 'Economy Comfort', '59.00'),
-  (3, 'Business', '99.00'),
-  (4, 'First', '199.00');
+  (1, 'Economy', 0.00::MONEY),
+  (2, 'Economy Comfort', 59.00::MONEY),
+  (3, 'Business', 99.00::MONEY),
+  (4, 'First', 199.00::MONEY);
