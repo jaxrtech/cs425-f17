17c690a08e62cc50a6fe3ec70a1e7eb5f6b35e5f
Josh Bowden <josbow@gmail.com>
2017-12-04 14:27:37 -0600

    chore: add `reset_db.sh` for linux

---
+++ reset_db.sh
@@ -0,0 +1,10 @@
+psql -p 5432 -d aero -U postgres \
+  -f ./schema/down.sql \
+  -f ./schema/up.sql \
+  -f ./sample_data/aero_class.sql \
+  -f ./sample_data/aero_airline.sql \
+  -f ./sample_data/aero_airport.sql \
+  -f ./sample_data/aero_address.sql \
+  -f ./sample_data/aero_flight_schedule.sql \
+  -f ./sample_data/aero_flight.sql \
+  -f ./sample_data/aero_flight_class.sql
